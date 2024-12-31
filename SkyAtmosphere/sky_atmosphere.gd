299a3578c0d9fed9cb7b374d38699ec9fc0131d4
ChefCthecookofu <connorlyles23@gmail.com>
2024-12-30 21:07:41 -0500

    Dynamic day/night + rebuild

--- SkyAtmosphere/sky_atmosphere.gd
+++ SkyAtmosphere/sky_atmosphere.gd
@@ -10,15 +10,18 @@ extends Node3D
 @export var night_top_color = Color(0.0, 0.0, 0.1)
 @export var night_horizon_color = Color(0.06, 0.14, 0.19)
 
+@export var sun_energy_day = 1.0
+@export var sun_energy_night = 0
+
 var time_dict = Time.get_datetime_dict_from_system()
 var time_of_day = time_dict["second"]
 var norm_time_of_day = remap(time_of_day, 0, 86400, 0, 1)
 var debug = true
 
-func _process(delta: float):
+func _process(_delta):
 	# Get time in seconds and normalize it (0-1)
 	time_dict = Time.get_datetime_dict_from_system()
-	if debug:
+	if debug and OS.has_feature("editor"):
 		time_of_day = time_dict["second"]
 		norm_time_of_day = remap(time_of_day, 0, 60, 0, 1)
 	else:
@@ -33,7 +36,6 @@ func _process(delta: float):
 	if sky:
 		# Calculate the blending factor for the top and horizon colors using a cosine function.
 		var blend_factor = (cos(norm_time_of_day * PI * 2) + 1) / 2  # This will oscillate between 0 and 1
-		print(blend_factor)
 
 		#Blend sky colors
 		var top_color : Color
@@ -44,13 +46,13 @@ func _process(delta: float):
 			horizon_color = lerp(day_horizon_color, twilight_horizon_color, blend_factor / 0.5)
 		# Blend between twilight and midnight
 		else:
-			top_color = lerp(twilight_top_color, night_top_color, blend_factor / 0.5)
-			horizon_color = lerp(twilight_horizon_color, night_horizon_color, blend_factor / 0.5)
+			top_color = lerp(twilight_top_color, night_top_color, remap(blend_factor, 0.5, 1, 0, 1))
+			horizon_color = lerp(twilight_horizon_color, night_horizon_color, remap(blend_factor, 0.5, 1, 0, 1))
 		sky.sky_top_color = top_color
 		sky.sky_horizon_color = horizon_color
+		sky.ground_bottom_color = top_color
+		sky.ground_horizon_color = horizon_color
 
 
 		# Simulate sun intensity for day/night cycle
-		var sun_energy_day = 1.0
-		var sun_energy_night = 0.2
-		sun.light_energy = lerp(sun_energy_night, sun_energy_day, clamp(cos(PI * norm_time_of_day) / 2 + 1, 0.0, 1.0))
+		sun.light_energy = remap(1 - blend_factor, 0, 1, sun_energy_night, sun_energy_day)
